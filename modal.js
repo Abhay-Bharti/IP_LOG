ad680d10a54c3d00ee677b8eb89a3f554ff44623
Abhay Bharti <abhaybharti2123@gmail.com>
2024-08-30 19:45:45 +0530

    date added

--- modal.js
+++ modal.js
@@ -1,11 +1,10 @@
-const {model, Schema} = require('mongoose');
+const { model, Schema } = require('mongoose');
 
-const ipSchema  = new Schema({
-    ip: {
-        type: String,
-    }
-}, {timestamps: true});
+const ipSchema = new Schema({
+    ip: { type: String },
+    timestamp: { type: Date, default: Date.now },
+});
 
-const IP = model("IP" , ipSchema );
+const IP = model("IP", ipSchema);
 
 module.exports = IP;
